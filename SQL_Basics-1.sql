a4cdaed9d00e20651226f917aa247e0bb7bbe9de
Jaiharish-passion07 <78978975+Jaiharish-passion07@users.noreply.github.com>
2022-03-19 11:38:09 +0530

    Update SQL_Basics-1.sql

--- SQL_Basics-1.sql
+++ SQL_Basics-1.sql
@@ -1,28 +1,28 @@
    --👉✨SQL ORDER✨👈:
 
 /*
-	  🌻SELECT🌻
+	     🌻SELECT🌻
 		  |
 		  👇
-	  🌻FROM🌻
+	      🌻FROM🌻
 		  |
 		  👇
-	   🌻JOIN🌻
+	      🌻JOIN🌻
 		  |
 		  👇
-	  🌻WHERE🌻
+	      🌻WHERE🌻
 		  |
 		  👇
-	  🌻GROUP BY🌻
+	     🌻GROUP BY🌻
 		  |
 		  👇
-	  🌻HAVING🌻
+	     🌻HAVING🌻
 		  |
 		  👇
-	  🌻ORDERBY🌻
+	     🌻ORDERBY🌻
 		  |
 		  👇
-	  🌻LIMIT🌻
+	      🌻LIMIT🌻
 
 */
 
@@ -180,7 +180,5 @@ SELECT customer_id,
 FROM store
 WHERE customer_name 
 LIKE '___a%';
-
-
 
 				
